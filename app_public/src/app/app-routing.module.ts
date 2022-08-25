109f97672501d738848abf1a4f662d46cac9a25b
officelaptop <rana1506@gmail.com>
2022-08-25 20:44:35 +0600

    downloded from remote master and pushed to main

--- app_public/src/app/app-routing.module.ts
+++ app_public/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
-const routes: Routes = [{
-  path: '',
-  component: HomeComponent
-}
+const routes: Routes = [
+  {  path: 'home',  component: HomeComponent },
+  {  path: 'reviews',  component: ReviewsComponent }
 ];
 
 @NgModule({
--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -4,17 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FrameworkComponent } from './framework/framework.component';
+
+import { ReviewDataService } from './review-data.service';
+import { ReviewsComponent } from './reviews/reviews.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FrameworkComponent,
+    ReviewsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ReviewDataService],
+  bootstrap: [FrameworkComponent]
 })
 export class AppModule { }
