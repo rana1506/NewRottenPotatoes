29f49a7c6e69083dd12cac63113271f843f4da8e
officelaptop <rana1506@gmail.com>
2022-08-25 19:30:46 +0600

    local file commited before pull

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -3,24 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { FrameworkComponent } from './framework/framework.component';
-
-import { ReviewDataService } from './review-data.service';
-import { ReviewsComponent } from './reviews/reviews.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    FrameworkComponent,
-    ReviewsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [ReviewDataService],
-  bootstrap: [FrameworkComponent]
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
